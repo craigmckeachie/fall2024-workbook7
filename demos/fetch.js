f21228ca708f69c346e7790fa599f4fdd44e7f1c
Craig McKeachie <craigdmckeachie@gmail.com>
2024-11-18 15:58:35 -0500

    fetch 2

--- demos/fetch.js
+++ demos/fetch.js
@@ -1,15 +1,19 @@
 "use strict";
 
+//you get a promise object back which
+//you hold as collateral for eventually getting the data you requested
+let promise = fetch("https://jsonplaceholder.typicode.com/users");
 
+promise.then(parseJSON).then(displayUsers);
 
-let promise = fetch("https://jsonplaceholder.typicode.com/users")
-
-function displayUsers(users){
- console.log(users);
+//takes the body (after the blank line) of the http response
+//and calls JSON.parse() on it
+function parseJSON(response) {
+  let arrayOfUserObjects = response.json(); //response.body's json string
+  return arrayOfUserObjects;
 }
 
-function parseJSON(response){
-    return response.json(); 
+//recieves the data you asked for and does something to display it
+function displayUsers(users) {
+  console.log(users);
 }
-
-promise.then(parseJSON).then(displayUsers)
