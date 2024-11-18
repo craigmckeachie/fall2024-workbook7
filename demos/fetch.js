e57851a892379265470d81c44af69611679313b4
Craig McKeachie <craigdmckeachie@gmail.com>
2024-11-18 16:02:56 -0500

    fetch comments

--- demos/fetch.js
+++ demos/fetch.js
@@ -6,11 +6,11 @@ let promise = fetch("https://jsonplaceholder.typicode.com/users");
 
 promise.then(parseJSON).then(displayUsers);
 
-//takes the body (after the blank line) of the http response
-//and calls JSON.parse() on it
+//takes the body (after the blank line) in the http response
+//and calls JSON.parse() on it to transform it from a json string to an array of objects
 function parseJSON(response) {
   let arrayOfUserObjects = response.json(); //response.body's json string
-  return arrayOfUserObjects;
+  return arrayOfUserObjects; //returning the array is important because it keeps the promise chain going, i.e. the data will flow into the next then block
 }
 
 //recieves the data you asked for and does something to display it
