11af27dbdef322ab136855781cfeb145c957b1dc
Craig McKeachie <craigdmckeachie@gmail.com>
2024-11-21 16:02:28 -0500

    northwind workshop started

--- demos/fetch-async-await-error-handling.js
+++ demos/fetch-async-await-error-handling.js
@@ -7,6 +7,8 @@ async function getData() {
     if (response.ok) {
       let data = await response.json(); //turn JSON string in the body into a JavaScript array of objects
       console.log(data);
+    }else{
+      throw new Error(`The response came back with an error : ${response.status} ${response.message} `)
     }
   } catch (error) {
     console.log("Error occured: " + error.message);
